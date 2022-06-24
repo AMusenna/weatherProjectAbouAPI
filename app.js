2eeefed6122727f070d78486c0e19c9ec4ec979e
Amusenna <ahmet@visiosoft.com.tr>
2022-06-24 16:53:42 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const https = require("node:https");
+const app = express();
+const port = 3000
+
+app.get("/", function(req, res){
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Paris&units=metric&appid=65441206f989bc53319e2689fccdc638";
+
+  https.get(url, function(response){
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data)
+      const temp = weatherData.main.temp
+      const weatherDescription = weatherData.weather[0].description
+      const icon = weatherData.weather[0].icon
+      const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+      res.write("<p>The weather currently " + weatherDescription + "</p>")
+      res.write("<h1> The tempreture in London is " + temp + " degrees Celcius. </h1>")
+      res.write("<img src=" + imageUrl + " >")
+      res.send();
+    })
+
+  });
+})
+
+
+
+
+app.listen(port, function() {
+  console.log('example app listening on port ' + port);
+});
